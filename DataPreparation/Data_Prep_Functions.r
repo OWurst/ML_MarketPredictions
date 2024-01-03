0906d29d70a10d807168535247850fb636ff68ca
Owen Wurst <OMW3@pitt.edu>
2024-01-03 16:31:52 -0500

    Begin data transformation

---
+++ DataPreparation/Data_Prep_Functions.r
@@ -0,0 +1,14 @@
+# This file contains functions used to prep data for ML, some are likely to be rather verbose, so I do not want them to all be in the Rmd file
+
+## Function to prep data immediately after reading in csv, calculate percent changes, include volume, ticker, industry, date.
+prep_df_post_csv_read <- function(df, ticker, industry) {
+  return_df <- data.frame(
+    Ticker = ticker,
+    Industry = industry,
+    Date = df$Date,
+    Volume = df$Volume,
+    Adj_Pct_Change = ((df$"Adj Close" - df$Open) / df$Open *100),
+    Pct_Change = ((df$Close-df$Open) / df$Open *100)
+  )
+  return(return_df)
+}
