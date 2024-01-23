5c797ece12d858cc95f9a0c22ce9165499a102ef
Owen Wurst <OMW3@pitt.edu>
2024-01-23 12:15:21 -0500

    Begin model training

--- Model_Training_And_Eval/ModelTrainingFunctions.r
+++ Model_Training_And_Eval/ModelTrainingFunctions.r
@@ -15,6 +15,18 @@ drop_unwanted_pct_data <- function(df, adjusted = FALSE){
   } else {
     df_filtered <- df %>% select(-starts_with("Adj"), -Actual_AdjPerChange)
   }
-  
+
   return(df_filtered)
+}
+
+prep_df_list <- function(files_list, adjusted = FALSE){
+  return_list <- list()
+  
+  for (name in names(files_list)) {
+    df <- read(files_list[[name]])
+    df <- drop_unwanted_pct_data(df, adjusted = adjusted)
+    
+    return_list[[name]] <- df
+  }
+  return(return_list)
 }
